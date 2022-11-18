7e3f8b5afea259078f8f8e6d50db74ddf2ca86f7
zemaciel <joemacbr@gmail.com>
2022-11-18 19:37:20 +0000

    Add features to test files

--- script2.js
+++ script2.js
@@ -1,9 +1,24 @@
-// define the object for the question entity
+
 let movieQuote = {
     title: 'Show me the money',
-    alternatives: ['MovieA', 'MovieB', 'MovieC', 'MovieD'],
+    filmTitles: ['MovieA', 'MovieB', 'MovieC', 'MovieD'],
     correctAnswer: 1
   };
+
+
+//  movieQuote from array to object: 
+
+// let movieQuote =[
+//     {
+//         title: "I'm going to make him an offer he can't refuse.",
+//         a: "The Negotiator",
+//         b: "The Firm",
+//         c: "The Godfather",
+//         d: "The Departed",
+//         correct: "c",
+//     },
+// ];
+
   
 
   let titleDiv = document.getElementById('title');
@@ -15,7 +30,30 @@ let movieQuote = {
     
     let titleDiv = document.getElementById('title');
     titleDiv.textContent = movieQuote.title;
-  
+    
+    let films = document.querySelectorAll('.film');
+    // console.log(films);
+
+    //interating -element is the list items - index is the position
+    films.forEach(function(element, index){
+        element.textContent = q.filmTitles[index];
+        element.addEventListener('click', function(){
+            // element.textContent = "oba"
+            if(q.correctAnswer == index) {
+                console.log('Right!');
+            }
+            else {
+                console.log('Wrong, dude!');
+            }
+        })
+     });
   }
- 
+
   showQuote(movieQuote);
+
+  let btn = document.getElementById('btn'); 
+
+  btn.addEventListener('click', function(){
+    console.log('Clicou!');
+
+  })
