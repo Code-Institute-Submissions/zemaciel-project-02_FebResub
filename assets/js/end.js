5cea438bd48a0e99d90b1c897fe1e01bca4f0ca1
zemaciel <joemacbr@gmail.com>
2023-02-01 22:06:48 +0000

    Add feature -  feedback based on score

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,16 +1,14 @@
-const username = document.getElementById('username');
-const saveScoreBtn = document.getElementById('saveScoreBtn');
-const finalScore = document.getElementById('finalScore');
-const mostRecentScore = localStorage.getItem('mostRecentScore');
-
-const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
-
+const username = document.getElementById("username");
+const saveScoreBtn = document.getElementById("saveScoreBtn");
+const finalScore = document.getElementById("finalScore");
+const mostRecentScore = localStorage.getItem("mostRecentScore");
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 const MAX_HIGH_SCORES = 5;
 
 finalScore.innerText = mostRecentScore;
 
 //if there is no input of user name, buttom is disabled
-username.addEventListener('keyup', () => {
+username.addEventListener("keyup", () => {
     saveScoreBtn.disabled = !username.value;
 });
 
@@ -25,21 +23,39 @@ saveHighScore = (e) => {
     highScores.sort((a, b) => b.score - a.score);
     highScores.splice(5);
 
-    localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign('../highscores.html');
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    window.location.assign("highscores.html");
 };
 
+// check palyers score
 let thisGameScore = parseInt(mostRecentScore);
 console.log("This is the end " + mostRecentScore);
 console.log(typeof (mostRecentScore))
 console.log(thisGameScore + (typeof (thisGameScore)))
 
-if (thisGameScore <= 20) {
-    console.log("brutal");
+//custom feedback based on score
+feedbackTxtResul = ""
+
+if (thisGameScore <= 40) {
+    feedbackTxtResul = "Bad Luck! Try again?";
+    //feedback image for low score
+    const imgResultOk = document.createElement("img");
+    imgResultOk.src = "assets/images/result_bummer.gif";
+    document.getElementById("feedbackImage").appendChild(imgResultOk);
+} else if (thisGameScore >= 50 && thisGameScore <= 70) {
+    feedbackTxtResul = "Thanks for playing";
+    // feedback image for avarage score
+    const imgResultOk = document.createElement("img");
+    imgResultOk.src = "assets/images/result_ok.gif";
+    document.getElementById("feedbackImage").appendChild(imgResultOk);
+} else {
+    feedbackTxtResul = "Wow. You're a film buff!";
+    // feedback image for greater scores
+    const imgResultOk = document.createElement("img");
+    imgResultOk.src = "assets/images/result_great.gif";
+    document.getElementById("feedbackImage").appendChild(imgResultOk);
 }
-else if (thisGameScore >= 30 && thisGameScore <= 80) {
-    console.log("Thanks for playing")
-}
-else {
-    console.log("Excelent. /n Hasta la vista baby")
-}
+
+feedbackMessage = document.getElementById("feedbackMessage");
+txt = document.createTextNode(feedbackTxtResul);
+feedbackMessage.appendChild(txt);
